cc8ecebd66fdf6bc9f24659f289fba6af200a6f2
soasme <soasme@gmail.com>
2016-05-26 17:41:30 +0800

    Add executor pools.

---
+++ blackgate/executor_pools.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from concurrent.futures import ThreadPoolExecutor
+
+class ExecutorPools(object):
+
+    def __init__(self):
+        self.pools = {}
+
+    def register_pool(self, group_key, max_workers=1):
+        self.pools[group_key] = ThreadPoolExecutor(max_workers=max_workers)
+
+    def get_executor(self, group_key):
+        if group_key not in self.pools:
+            raise Exception("Pool not registerd")
+        return self.pools[group_key]
