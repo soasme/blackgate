b4fea3b6fc2c5874609a8eff101d447fbf3fe8aa
soasme <soasme@gmail.com>
2016-05-27 11:51:32 +0800

    Add configure component API.

--- blackgate/component.py
+++ blackgate/component.py
@@ -8,3 +8,21 @@ class Component(object):
     def __init__(self):
         self.pools = ExecutorPools()
         self.circuit_beaker = CircuitBeaker()
+        self.configurations = {}
+
+    def set(self, key, value):
+        self.configurations[key] = value
+
+    def add(self, key, value):
+        self.configurations.setdefault(key, [])
+        if key in self.configurations:
+            assert isinstance(self.configurations[key], list)
+        self.configurations[key].append(value)
+
+    def delete(self, key):
+        del self.configurations[key]
+
+    def install(self):
+        if 'executor_pool' in self.configurations:
+            for executor_pool in self.configurations['executor_pool']:
+                self.pools.register_pool(executor_pool['group_key'], executor_pool['max_workers'])
