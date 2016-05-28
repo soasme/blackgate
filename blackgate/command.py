af6ddbf6c7e1b4c4338f6b02b29ff7b30b661156
soasme <soasme@gmail.com>
2016-05-28 10:46:38 +0800

    Get circuit beaker getter from component.

--- blackgate/command.py
+++ blackgate/command.py
@@ -8,7 +8,7 @@ import requests
 from tornado import gen
 from tornado.ioloop import IOLoop
 
-from .core import component
+from blackgate.core import component
 
 logger = logging.getLogger(__name__)
 
@@ -29,7 +29,14 @@ class Command(object):
     def queue(self):
         # TODO: implement cache machenism.
 
-        if not component.circuit_beaker.is_allow_request(self.group_key, self.command_key):
+        circuit_beaker = component.get_circuit_beaker(
+            self.command_key,
+            self.group_key,
+            component.circuit_beaker_impl,
+            **component.circuit_beaker_options,
+        )
+
+        if not circuit_beaker.allow_request(self.group_key, self.command_key):
             future = self.fallback()
             raise gen.Return(future)
 
@@ -37,21 +44,21 @@ class Command(object):
 
         try:
             future = yield executor.submit(self.run)
-            component.circuit_beaker.mark_success(self.group_key, self.command_key)
+            circuit_beaker.mark_success(self.group_key, self.command_key)
 
         except component.pools.PoolFull:
             future = self.fallback()
-            component.circuit_beaker.mark_reject(self.group_key, self.command_key)
+            circuit_beaker.mark_reject(self.group_key, self.command_key)
             logger.error('type: pool_full')
 
         except component.pools.ExecutionTimeout:
             future = self.fallback()
-            component.circuit_beaker.mark_timeout(self.group_key, self.command_key)
+            circuit_beaker.mark_timeout(self.group_key, self.command_key)
             logger.error('type: execution_timeout')
 
         except Exception as exception:
             future = self.fallback()
-            component.circuit_beaker.mark_failure(self.group_key, self.command_key)
+            circuit_beaker.mark_failure(self.group_key, self.command_key)
             logger.error('type: execution_fail, reason: %s', exception.message)
 
         raise gen.Return(future)
