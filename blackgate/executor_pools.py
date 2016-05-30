1a6aa5586fa208339c6ce5f549656be02dfa9902
soasme <soasme@gmail.com>
2016-05-30 17:31:05 +0800

    register ioloop.

--- blackgate/executor_pools.py
+++ blackgate/executor_pools.py
@@ -9,9 +9,10 @@ class ExecutorPools(object):
     def __init__(self):
         self.pools = {}
 
-    def register_pool(self, group_key, max_size=10, max_workers=10):
+    def register_pool(self, group_key, max_size=10, max_workers=10, ioloop=None):
         executor = QueueExecutor(pool_key=group_key, max_size=max_size, max_workers=max_workers)
-        IOLoop.current().spawn_callback(executor.consume)
+        ioloop = ioloop or IOLoop.current()
+        ioloop.spawn_callback(executor.consume)
         self.pools[group_key] = executor
 
     def get_executor(self, group_key):
