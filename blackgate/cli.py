0080f08df36b1d4665a7dd0131b76bc2b30a07ed
soasme <soasme@gmail.com>
2016-06-23 17:05:54 +0800

    finish cli code.

--- blackgate/cli.py
+++ blackgate/cli.py
@@ -3,15 +3,38 @@
 import click
 
 from blackgate.core import component
+from blackgate.config import parse_yaml_config
+from blackgate.config import read_yaml_config
+from blackgate.config import read_default_config
 from blackgate.server import run
 
+
 @click.group()
-def main():
-    # README CONFIG
-    component.install_from_config(config)
+@click.option('-c', '--config', default='')
+@click.pass_context
+def main(ctx, config):
+    if not config:
+        config = read_default_config()
+    else:
+        config = read_yaml_config(config)
+
+    if not config:
+        ctx.fail('config not found.')
+
+    try:
+        config = parse_yaml_config(config)
+    except ValueError:
+        ctx.fail('config is not valid yaml.')
+
+    ctx.obj['config'] = config
+
 
 @main.command()
-def start():
+@click.pass_context
+def start(ctx):
+    config = ctx.obj['config']
+    component.configurations = config
+    component.install()
     run(config.get('port', 9654))
 
 
