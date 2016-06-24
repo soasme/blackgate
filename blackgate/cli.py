8b752e1db53e1174edba8623ba3f725ceecd5ad4
soasme <soasme@gmail.com>
2016-06-24 14:48:34 +0800

    implement start/stop/restart.

--- blackgate/cli.py
+++ blackgate/cli.py
@@ -6,7 +6,7 @@ from blackgate.core import component
 from blackgate.config import parse_yaml_config
 from blackgate.config import read_yaml_config
 from blackgate.config import read_default_config
-from blackgate.server import run
+from blackgate.server import Server
 
 
 @click.group()
@@ -31,12 +31,53 @@ def main(ctx, config):
 
 
 @main.command()
+@click.option('--daemon/--no-daemon', default=True)
+@click.option('--pidfile', default='/var/run/blackgate.pid')
 @click.pass_context
-def start(ctx):
+def start(ctx, daemon, pidfile):
     config = ctx.obj['config']
     component.configurations = config
     component.install()
-    run(config.get('port', 9654))
+
+    if pidfile != config['pidfile']:
+        pidfile = config['pidfile']
+
+    daemon = Server(pidfile)
+
+    if daemon:
+        daemon.start()
+    else:
+        daemon.run()
+
+@main.command()
+@click.option('--pidfile', default='/var/run/blackgate.pid')
+@click.pass_context
+def stop(ctx, pidfile):
+    config = ctx.obj['config']
+    component.configurations = config
+    component.install()
+
+    if pidfile != config['pidfile']:
+        pidfile = config['pidfile']
+
+    daemon = Server(pidfile)
+    daemon.stop()
+
+
+@main.command()
+@click.option('--pidfile', default='/var/run/blackgate.pid')
+@click.pass_context
+def restart(ctx, pidfile):
+    config = ctx.obj['config']
+    component.configurations = config
+    component.install()
+
+    if pidfile != config['pidfile']:
+        pidfile = config['pidfile']
+
+    daemon = Server(pidfile)
+    daemon.restart()
+
 
 
 if __name__ == '__main__':
