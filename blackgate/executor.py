d358439a374cfb07c6d7125058c14bc2374e301a
soasme <soasme@gmail.com>
2016-06-28 14:37:01 +0800

    Define async item.

--- blackgate/executor.py
+++ blackgate/executor.py
@@ -23,6 +23,25 @@ class WorkItem(object):
         else:
             self.future.set_result(result)
 
+class AsyncItem(object):
+
+    executor = ThreadPoolExecutor(20)
+
+    def __init__(self, future, fn, args, kwargs):
+        self.future = future
+        self.fn = fn
+        self.args = args
+        self.kwargs = kwargs
+
+    @gen.coroutine
+    def run(self):
+        try:
+            result = yield self.fn(*self.args, **self.kwargs)
+        except Exception:
+            self.future.set_exc_info(sys.exc_info())
+        else:
+            self.future.set_result(result)
+
 class QueueExecutor(object):
 
     def __init__(self, pool_key, max_size, max_workers):
@@ -30,11 +49,10 @@ class QueueExecutor(object):
         self._max_size = max_size
         self._max_workers = max_workers
         self._work_queue = queues.Queue(max_size)
-        self._work_executor = ThreadPoolExecutor(max_workers)
 
     def submit(self, fn, *args, **kwargs):
         future = Future()
-        item = WorkItem(future, fn, args, kwargs)
+        item = AsyncItem(future, fn, args, kwargs)
         self._work_queue.put_nowait(item)
         return future
 
@@ -43,6 +61,6 @@ class QueueExecutor(object):
         while True:
             try:
                 item = yield self._work_queue.get()
-                yield self._work_executor.submit(item.run)
+                yield item.run()
             finally:
                 self._work_queue.task_done()
