ff1236c19676e8df70d97753a5222f40ed815397
soasme <soasme@gmail.com>
2016-06-29 16:32:58 +0800

    Remove core in server module.

--- blackgate/server.py
+++ blackgate/server.py
@@ -6,16 +6,20 @@ from tornado.ioloop import IOLoop
 from tornado.web import Application
 from tornado.httpserver import HTTPServer
 
-from blackgate.core import component
 from blackgate.daemon import Daemon
 
 
 class Server(Daemon):
 
+    def set_app(self, app):
+        self._app = app
+
+    def set_port(self, port):
+        self._port = port
+
     def run(self):
-        app = Application(component.urls)
-        server = HTTPServer(app)
-        server.listen(component.configurations.get('port', 9654))
+        server = HTTPServer(self._app)
+        server.listen(self._port)
         try:
             IOLoop.current().start()
         except KeyboardInterrupt:
