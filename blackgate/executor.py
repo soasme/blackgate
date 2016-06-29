bd9fd3efd108dec50e44997d52db571d1865aed7
soasme <soasme@gmail.com>
2016-06-29 16:23:51 +0800

    define executor pool in executor module.

--- blackgate/executor.py
+++ blackgate/executor.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 
 import sys
+
 from concurrent.futures import ThreadPoolExecutor
 from tornado import gen, queues
 from tornado.concurrent import Future, run_on_executor
+
+from blackgate.http_client import fetch, handle_client_error
 
 class WorkItem(object):
 
@@ -44,10 +47,9 @@ class AsyncItem(object):
 
 class QueueExecutor(object):
 
-    def __init__(self, pool_key, max_size, max_workers):
+    def __init__(self, pool_key, max_size):
         self._pool_key = pool_key
         self._max_size = max_size
-        self._max_workers = max_workers
         self._work_queue = queues.Queue(max_size)
 
     def submit(self, fn, *args, **kwargs):
@@ -64,3 +66,33 @@ class QueueExecutor(object):
                 yield item.run()
             finally:
                 self._work_queue.task_done()
+
+
+class ExecutorPools(object):
+
+    def __init__(self):
+        self.pools = {}
+
+    def register_pool(self, ioloop, group_key, max_size=10):
+        executor = QueueExecutor(pool_key=group_key, max_size=max_size)
+        ioloop.spawn_callback(executor.consume)
+        self.pools[group_key] = executor
+        return executor
+
+    def get_executor(self, group_key):
+        if group_key not in self.pools:
+            raise Exception("Pool not registerd: %s" % group_key)
+        return self.pools[group_key]
+
+    @gen.coroutine
+    def execute(self, request, options=None):
+        group_key = options.get('name')
+        executor = self.get_executor(group_key)
+        options = options or {}
+        try:
+            submit_data = dict(request=request, options=options)
+            result = yield executor.submit(fetch, **submit_data)
+            raise gen.Return(result)
+        except Exception as exc:
+            result = handle_client_error(exc)
+            raise gen.Return(result)
