d8547f9d375b29c7033d8bb7d3f24235f4f1270e
soasme <soasme@gmail.com>
2016-06-29 09:41:07 +0800

    rewrite blackgate command to functions.

--- blackgate/command.py
+++ blackgate/command.py
@@ -15,121 +15,81 @@ from blackgate.core import component
 
 logger = logging.getLogger(__name__)
 
-class Command(object):
+def fallback(reason=''):
+    """Fallback response."""
+    return dict(
+        status_code=502,
+        reason='Bad Gateway',
+        headers={
+            'Content-Type': 'application/json',
+        },
+        content=json.dumps({
+            'code': 502,
+            'message': str(reason) or 'gateway is using fallback mechanism',
+            'errors': []
+        })
+    )
 
-    def __init__(self, request, proxy):
-        self.proxy = proxy
-        self.request = request
-        self.response = {}
-
-    @gen.coroutine
-    def async(self):
-        client = AsyncHTTPClient()
-        self.before_request()
-        request = HTTPRequest(
-            url='%s?%s' % (self.request['url'], urlencode(self.request['params'])),
-            method=self.request['method'].upper(),
-            headers=self.request['headers'],
-            body=self.request['data'] if self.request['method'].upper() != 'GET' else None,
-            connect_timeout=self.options.get('connect_timeout_seconds'),
-            request_timeout=self.options.get('timeout_seconds'),
+@gen.coroutine
+def fetch(request, options=None):
+    """Fetch request."""
+    client = AsyncHTTPClient()
+    options = options or {}
+    request = HTTPRequest(
+        url='%s?%s' % (request['url'], urlencode(request['params'])),
+        method=request['method'].upper(),
+        headers=request['headers'],
+        body=request['data'] if request['method'].upper() != 'GET' else None,
+        connect_timeout=options.get('connect_timeout_seconds'),
+        request_timeout=options.get('timeout_seconds'),
+    )
+    try:
+        resp = yield client.fetch(request)
+        response = dict(
+            status_code=resp.code,
+            reason=resp.reason,
+            headers=dict(resp.headers),
+            content=resp.body,
         )
-        try:
-            resp = yield client.fetch(request)
-            self.response = dict(
-                status_code=resp.code,
-                reason=resp.reason,
-                headers=dict(resp.headers),
-                content=resp.body,
-            )
-        except HTTPError as error:
-            self.response = dict(
-                status_code=error.code,
-                reason=error.response.reason,
-                headers=dict(error.response.headers),
-                content=resp.response.body,
-            )
-        self.after_response()
-        raise gen.Return(self.response)
-
-    def fallback(self, reason=''):
-        return dict(
-            status_code=502,
-            reason='Bad Gateway',
-            headers={
-                'Content-Type': 'application/json',
-            },
-            content=json.dumps({
-                'code': 502,
-                'message': str(reason) or 'gateway is using fallback mechanism',
-                'errors': []
-            })
+    except HTTPError as error:
+        response = dict(
+            status_code=error.code,
+            reason=error.response.reason,
+            headers=dict(error.response.headers),
+            content=resp.response.body,
         )
+    raise gen.Return(response)
 
-    @property
-    def options(self):
-        return dict(
-            group_key=self.proxy['name'],
-            command_key='proxy',
-            timeout_seconds=self.proxy.get('timeout_seconds') or 30,
-            connect_timeout_seconds=self.proxy.get('connect_timeout_seconds') or 3,
-            timeout_enabled=bool(self.proxy.get('timeout_enabled')),
-       )
 
-    @property
-    def circuit_beaker(self):
-        return component.get_circuit_beaker(
-            self.options.get('command_key'),
-            self.options.get('group_key'),
-            component.circuit_beaker_impl,
-            **component.circuit_beaker_options
-        )
+def on_socket_error(error):
+    return fallback('gateway reject due to broken upstream connection.')
 
-    @gen.coroutine
-    def queue(self):
-        # TODO: implement cache machenism.
-        circuit_beaker = self.circuit_beaker
-        group_key = self.options.get('group_key')
-        executor = component.pools.get_executor(group_key)
+def on_queue_full(error):
+    return fallback('gateway reject due to too many connections.')
 
-        if not circuit_beaker.allow_request():
-            result = self.fallback('gateway reject due to circuit beaker open.')
-            logger.error('type: circuit_beaker_reject')
-            raise gen.Return(result)
+def on_gen_timeout(error):
+    return fallback('gateway reject due to timeout')
 
-        try:
-            result = yield executor.submit(self.async)
-            circuit_beaker.mark_success() # FIXME: this should be async, possibly network error
+COMMAND_ERRORS = {
+    socket.error: on_socket_error,
+    queues.QueueFull: on_queue_full,
+    gen.TimeoutError: on_gen_timeout,
+}
 
-        except socket.error:
-            result = self.fallback('gateway failed to connect upstream.')
-            circuit_beaker.mark_timeout()
-            logger.error('type: socket_error')
+def handle_command_error(error):
+    if isinstance(error, COMMAND_ERRORS.keys()):
+        return COMMAND_ERRORS[error.__class__]
+    raise error
 
-        except queues.QueueFull:
-            result = self.fallback('gateway reject due to too many connections.')
-            circuit_beaker.mark_reject()
-            logger.error('type: pool_full')
+@gen.coroutine
+def queue(request, options=None):
+    options = options or {}
+    group_key = options.get('group_key')
+    executor = component.pools.get_executor(group_key)
 
-        except gen.TimeoutError:
-            result = self.fallback('gateway timeout on fetching upstream.')
-            circuit_beaker.mark_timeout()
-            logger.error('type: execution_timeout')
-
-        # FIXME: catch more exceptions: submit failure, wrong timeout type, mark_success run failed.
-
-        except Exception as exception: # FIXME: only network error should fallback, others crash it??
-            # XXX: use some error aggreation system like sentry to report errors???
-            result = self.fallback()
-            circuit_beaker.mark_failure()
-            import traceback
-            traceback.print_exc()
-
+    try:
+        submit_data = dict(request=request, options=options)
+        result = yield executor.submit(fetch, **submit_data)
         raise gen.Return(result)
-
-
-    def before_request(self):
-        return
-
-    def after_response(self):
-        return
+    except Exception as exc:
+        handle_command_error(exc)
