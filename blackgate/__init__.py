6117ce7441626e7bf180c32ced08c4f9c9f334e1
soasme <soasme@gmail.com>
2016-06-29 16:29:26 +0800

    cancel exporting components.

--- blackgate/__init__.py
+++ blackgate/__init__.py
@@ -1,11 +1,1 @@
 # -*- coding: utf-8 -*-
-
-__all__ = [
-    'HTTPProxy',
-    'Command',
-    'component',
-]
-
-from .core import component
-from .http_proxy import HTTPProxy
-from . import options
