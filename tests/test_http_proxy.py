2d2d45334f9e6ff9d7f4dee7f2ff35790cc9d4ec
soasme <soasme@gmail.com>
2016-06-29 16:25:17 +0800

    test http proxy.

--- tests/test_http_proxy.py
+++ tests/test_http_proxy.py
@@ -1,38 +1,168 @@
 # -*- coding: utf-8 -*-
 
+import json
+
+from mock import patch, MagicMock
 from tornado.web import Application
-from tornado.testing import AsyncHTTPTestCase, gen_test
-from blackgate import HTTPProxyCommand, HTTPProxy, component
+from tornado import gen
+from tornado.testing import AsyncHTTPTestCase
 
-class TestHTTPProxy(AsyncHTTPTestCase):
+from blackgate.component import Component
 
-    def setUp(self):
-        super(TestHTTPProxy, self).setUp()
-        component.install()
+
+class TestHTTPProxyFallbackDueToBrokenUpstreamConnection(AsyncHTTPTestCase):
 
     def get_app(self):
-        class ProxyCommand(HTTPProxyCommand):
-            def run(self):
-                return dict(
-                    status_code=200,
-                    reason='OK',
-                    headers={
-                        'Server': 'SomeServer',
-                        'Content-Encoding': 'gzip',
-                        'Content-Length': 100,
-                        'Set-Cookie': 'a=b',
-                    },
-                    content='SUCCESS'
-                )
-        return Application([(r'/', HTTPProxy, dict(command=ProxyCommand, proxy={}))])
+        component = Component()
+        component.configurations = {
+            'proxies': [
+                {
+                    'name': 'upstream',
+                    'upstream_url': 'http://xxxxx.upstream.test/',
+                    'request_path_regex': '/upstream/(.*)',
+                    'request_path_sub': '/\1',
+                }
+            ]
+        }
+        component.install()
+        return Application(component.urls)
 
 
-    def test_response(self):
-        resp = self.fetch('/?a=1')
-        assert resp.code == 200
-        assert resp.reason == 'OK'
-        assert resp.headers.get('Server') != 'SomeServer'
-        assert not resp.headers.get('Content-Encoding') # != gzip
-        assert resp.headers.get('Content-Length') == str(len('SUCCESS')) # != 100
-        assert resp.body == 'SUCCESS'
-        assert resp.headers.get('Set-Cookie') == 'a=b'
+    def test_response_socket_error(self):
+        resp = self.fetch('/upstream/this-is-from-test-case')
+        assert resp.code == 502
+        assert resp.reason == 'Bad Gateway'
+        assert resp.body == json.dumps(dict(
+            code=502,
+            message='gateway reject due to broken upstream connection.',
+            errors=[],
+        ))
+
+class TestHTTPProxyFallbackDueToTooManyConnections(AsyncHTTPTestCase):
+
+    def get_app(self):
+        component = Component()
+        component.configurations = {
+            'proxies': [
+                {
+                    'name': 'upstream',
+                    'upstream_url': 'http://xxxxx.upstream.test/',
+                    'request_path_regex': '/upstream/(.*)',
+                    'request_path_sub': '/\1',
+                    'pool_max_workers': 1,
+                    'pool_auto_spawn': False,
+                }
+            ]
+        }
+        component.install()
+        return Application(component.urls)
+
+    def test_response_pool_full(self):
+        # Sorry, I havn't find a proper way to test this behaviour :(
+        return
+        resp = self.fetch('/upstream/x')
+        resp = self.fetch('/upstream/y')
+        assert resp.code == 502
+        assert resp.reason == 'Bad Gateway'
+        assert resp.body == json.dumps(dict(
+            code=502,
+            message='gateway reject due to too many connections.',
+            errors=[],
+        ))
+
+
+class TestHTTPProxyRegexWithPrefix(AsyncHTTPTestCase):
+
+    def get_app(self):
+        component = Component()
+        component.configurations = {
+            'proxies': [
+                {
+                    'name': 'upstream',
+                    'upstream_url': 'http://upstream.test',
+                    'request_path_regex': r'/upstream/(.*)',
+                    'request_path_sub': r'/\1',
+                    'pool_max_workers': 1,
+                    'pool_auto_spawn': False,
+                }
+            ]
+        }
+        component.install()
+        return Application(component.urls)
+
+    def test_response_with_prefix(self):
+        mock_client = MagicMock()
+        @gen.coroutine
+        def _mock_fetch(request):
+            assert request.url == 'http://upstream.test/test'
+            raise gen.Return(MagicMock(code=200, reason='OK', headers={}, body='xxx'))
+        mock_client().fetch = _mock_fetch
+
+        with patch('blackgate.http_client.AsyncHTTPClient', mock_client):
+            resp = self.fetch('/upstream/test')
+            assert resp.code == 200
+
+
+
+class TestHTTPProxyRegexWithAnotherPrefix(AsyncHTTPTestCase):
+
+    def get_app(self):
+        component = Component()
+        component.configurations = {
+            'proxies': [
+                {
+                    'name': 'upstream',
+                    'upstream_url': 'http://upstream.test',
+                    'request_path_regex': r'/upstream/(.*)',
+                    'request_path_sub': r'/upstream2/\1',
+                    'pool_max_workers': 1,
+                    'pool_auto_spawn': False,
+                }
+            ]
+        }
+        component.install()
+        return Application(component.urls)
+
+    def test_response_with_prefix(self):
+        mock_client = MagicMock()
+        @gen.coroutine
+        def _mock_fetch(request):
+            assert request.url == 'http://upstream.test/upstream2/test'
+            raise gen.Return(MagicMock(code=200, reason='OK', headers={}, body='xxx'))
+        mock_client().fetch = _mock_fetch
+
+        with patch('blackgate.http_client.AsyncHTTPClient', mock_client):
+            resp = self.fetch('/upstream/test')
+            assert resp.code == 200
+
+
+class TestHTTPProxyParams(AsyncHTTPTestCase):
+
+    def get_app(self):
+        component = Component()
+        component.configurations = {
+            'proxies': [
+                {
+                    'name': 'upstream',
+                    'upstream_url': 'http://upstream.test',
+                    'request_path_regex': r'/upstream/(.*)',
+                    'request_path_sub': r'/\1',
+                    'pool_max_workers': 1,
+                    'pool_auto_spawn': False,
+                }
+            ]
+        }
+        component.install()
+        return Application(component.urls)
+
+    def test_response_with_prefix(self):
+        mock_client = MagicMock()
+        @gen.coroutine
+        def _mock_fetch(request):
+            assert request.url == 'http://upstream.test/test?hello=world&hello=mars'
+            raise gen.Return(MagicMock(code=200, reason='OK', headers={}, body='xxx'))
+        mock_client().fetch = _mock_fetch
+
+        with patch('blackgate.http_client.AsyncHTTPClient', mock_client):
+            resp = self.fetch('/upstream/test?hello=world&hello=mars')
+            assert resp.code == 200
