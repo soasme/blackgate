6aed51aa69919b0515619d6479172e59fc632df9
soasme <soasme@gmail.com>
2016-06-30 15:33:53 +0800

    Bump version: 0.2.0 → 0.2.1

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ test_requirements = [
 
 setup(
     name='blackgate',
-    version='0.2.0',
+    version='0.2.1',
     license='MIT',
     description="A set of utilities to build API gateway.",
     long_description=readme + '\n\n' + history,
