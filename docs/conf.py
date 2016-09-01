356fee5d516d59595e1486c71208a8e23a49536f
soasme <soasme@gmail.com>
2016-09-01 09:11:54 +0800

    add sidebars.

--- docs/conf.py
+++ docs/conf.py
@@ -156,7 +156,9 @@ html_static_path = ['_static']
 #html_use_smartypants = True
 
 # Custom sidebar templates, maps document names to template names.
-#html_sidebars = {}
+html_sidebars = {
+    '**': ['sidebarintro.html', 'sourcelink.html', 'globaltoc.html', 'relations.html', 'searchbox.html'],
+}
 
 # Additional templates that should be rendered to pages, maps page names to
 # template names.
